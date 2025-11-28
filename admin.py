bc892346b0e93e68722c367f34643af955302a18
AbrorCPP <abrorboltayev4843@gmail.com>
2025-11-28 15:12:02 +0500

    final

--- admin.py
+++ admin.py
@@ -1,14 +1,21 @@
+import re
+
+x1 = r"^[a-z0-9_-]{3,15}$" #Username
+x2 = r"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"  #Phone num
+
 class User:
-    def __init__(self,name,age,worky):
+    def __init__(self,name,phone,age,worky):
         self.age = age
+        self.phone = phone
         self.name = name
         self.worky = worky
 
 class Car:
-    def __init__(self,title,color,year):
+    def __init__(self,title,color,year,plate):
         self.title = title
         self.color = color
         self.year = year
+        self.plate = plate
 
 class Bir:
     def __init__(self, user, car):
@@ -22,17 +29,29 @@ class Admin:
         self.adb = []
 
     def addUser(self):
-        user1 = input("Enter the name : ")
+        while True:
+            user = input("Enter the name : ")
+            if not re.match(x1,user):
+                break
+            else:
+                print("Wrong username")
+        while True:
+            phone = input("Enter the phone : ")
+            if re.match(x2,phone):
+                break
+            else:
+                print("Invalid phone number")
         age = input("Enter the age : ")
         worky = input("Enter the working year : ")
-        su = User(user1,age,worky)
+        su = User(user,phone,age,worky)
         self.users.append(su)
 
     def addCar(self):
         car1 = input("Enter the car name : ")
         color = input("Enter the color : ")
         year = input("Enter the car year : ")
-        sc = Car(car1,color,year)
+        plate = input("Enter the plate : ")
+        sc = Car(car1,color,year,plate)
         self.cars.append(sc)
 
     def printUs(self):
@@ -40,14 +59,14 @@ class Admin:
         print("_"*40)
         for user in self.users:
             count+=1
-            print(f"{count}. {user.name} {user.age} {user.worky}")
+            print(f"{count}. {user.name} {user.phone} {user.age} {user.worky}")
 
     def printCars(self):
         count = 0
         print("_"*40)
         for car in self.cars:
             count+=1
-            print(f"{count}. {car.title} {car.color} {car.year}")
+            print(f"{count}. {car.title} {car.color} {car.year} {car.plate}")
 
     def birl(self):
         print("=="*20)
@@ -70,13 +89,26 @@ class Admin:
         print("="*30)
         for obj in self.adb:
             print(
-                f"{count}. {obj.user.name} {obj.user.age} {obj.user.worky} --> {obj.car.title} {obj.car.color} {obj.car.year}")
+                f"{count}. {obj.user.name} {obj.user.phone} {obj.user.age} {obj.user.worky} --> {obj.car.title} {obj.car.color} {obj.car.year} {obj.car.plate}")
             count += 1
     def user_edit(self):
         name = input("Enter user name : ")
         for i in self.users:
             if i.name == name:
-                i.name = input("Enter new name : ")
+                while True:
+                    user = input("Enter the name : ")
+                    if not re.match(x1, user):
+                        i.user = user
+                        break
+                    else:
+                        print("Wrong username")
+                while True:
+                    phone = input("Enter the phone : ")
+                    if re.match(x2, phone):
+                        i.phone = phone
+                        break
+                    else:
+                        print("Invalid phone number")
                 i.age = input("Enter new age : ")
                 i.worky = input("Enter new working year : ")
 
@@ -87,18 +119,19 @@ class Admin:
                 i.title = input("Enter new title : ")
                 i.color = input("Enter new color : ")
                 i.year = input("Enter new year : ")
+                i.plate = input("Enter new plate : ")
 
     def user_del(self):
-        name = input("Enter user name : ")
+        phone = input("Enter phone number : ")
         for i in self.users:
-            if i.name == name:
+            if i.phone == phone:
                 self.users.remove(i)
                 print("Removed successfully")
 
     def car_del(self):
-        title = input("Enter car name : ")
+        plate = input("Enter car plate : ")
         for i in self.cars:
-            if i.title == title:
+            if i.plate == plate:
                 self.cars.remove(i)
                 print("Removed successfully")
 
@@ -107,12 +140,12 @@ class Admin:
         print("=" * 30)
         for obj in self.adb:
             print(
-                f"{count}. {obj.user.name} {obj.user.age} {obj.user.worky} --> {obj.car.title} {obj.car.color} {obj.car.year}")
+                f"{count}. {obj.user.name} {obj.user.phone} {obj.user.age} {obj.user.worky} --> {obj.car.title} {obj.car.color} {obj.car.year} {obj.car.plate}")
             count += 1
         a1 = int(input("Enter the connection you want to delete : ")) - 1
         t = self.adb[a1]
-        user1 = User(t.user.name,t.user.age,t.user.worky)
-        car1 = Car(t.car.title,t.car.color,t.car.year)
+        user1 = User(t.user.name,t.user.phone,t.user.age,t.user.worky)
+        car1 = Car(t.car.title,t.car.color,t.car.year,t.car.plate)
         self.users.append(user1)
         self.cars.append(car1)
         self.adb.remove(t)
